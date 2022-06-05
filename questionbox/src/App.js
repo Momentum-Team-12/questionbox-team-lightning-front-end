b2035ac4ac8987ff370105e386492dd50ca6991c
Jess Merry <merryja.email@gmail.com>
2022-06-05 17:47:22 -0400

    question prompt form

--- questionbox/src/App.js
+++ questionbox/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
-import QuestionDisplay from './components/QuestionDisplay/QuestionDisplay';
+import {QuestionDisplay} from './components/QuestionDisplay/QuestionDisplay';
 import AnswersDisplay from './components/AnswersDisplay/AnswersDisplay'
+import { QuestionPrompt } from './components/QuestionPrompt/QuestionPrompt';
 
 function App() {
 
   return (
     <div className="App">
+      <QuestionPrompt />
       <QuestionDisplay />
       <AnswersDisplay />
     </div>
--- questionbox/src/components/QuestionDisplay/QuestionDisplay.jsx
+++ questionbox/src/components/QuestionDisplay/QuestionDisplay.jsx
@@ -2,20 +2,24 @@ import React from "react";
 import axios from "axios";
 import { useState, useEffect } from "react";
 
-
-function QuestionDisplay() {
-	const [question, setQuestion] = useState([]);
+export function QuestionDisplay() {
+	const [questions, setQuestions] = useState([]);
 	useEffect(() => {
 		axios
 			.get(`https://questionbox-team-lightning.herokuapp.com/api/questions/`)
 			.then((res) => {
 				console.log(res.data);
-				setQuestion(res.data);
+				setQuestions(res.data);
 			});
 	}, []);
 	return (
-		<h1> {question.description} </h1>
-	)
-}
-
-export default QuestionDisplay
+		<>
+			<div className="question-display">
+				{questions.map((eachQuestion, index) => {
+					return (		
+					<h1 key={index}> {eachQuestion.description} </h1>
+					)
+				})}
+			</div>
+		</>
+	)}
