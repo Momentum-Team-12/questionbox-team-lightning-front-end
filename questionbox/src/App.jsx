37e1f7218594557971059f93967ca54833765677
Stephen Reish <spreish@ncsu.edu>
2022-06-05 19:45:09 -0400

    More token stuff laid out

--- questionbox/src/App.jsx
+++ questionbox/src/App.jsx
@@ -1,18 +1,35 @@
 import React from 'react';
-// import useLocalStorageState from 'use-local-storage-state'
+import useLocalStorageState from 'use-local-storage-state';
 import './App.css';
 import ExistingUserSignIn from './components/login/ExistingUserSignIn/ExistingUserSignIn'
 import QuestionDisplay from './components/QuestionDisplay/QuestionDisplay';
 import AnswersList from './components/AnswersList/AnswersList'
 
 const App = () => {
+  //using local storage to hold onto token issued by API upon successful login
+  const [token, setToken] = useLocalStorageState('reactLibraryToken', '')
+  const [username, setUsername] = useLocalStorageState('reactLibraryUsername', '')
 
-  return (
-    <div className="App">
-      <ExistingUserSignIn />
+  const setAuth = (username, token) => {
+    setToken(token)
+    setUsername(username)
+  }
+
+  const isLoggedIn = username && token
+
+  if (!isLoggedIn) {
+    return <>
+      <ExistingUserSignIn setAuth={setAuth} />
       <QuestionDisplay />
       <AnswersList />
-    </div>
+    </>
+  }
+
+  return (
+    <>
+      <QuestionDisplay />
+      <AnswersList />
+    </>
   );
 }
 
