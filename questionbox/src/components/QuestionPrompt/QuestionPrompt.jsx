03bda6e32a948928e226bbf55cbef81196d6579e
Jess Merry <merryja.email@gmail.com>
2022-06-07 19:42:53 -0400

    form submit hits API

--- questionbox/src/components/QuestionPrompt/QuestionPrompt.jsx
+++ questionbox/src/components/QuestionPrompt/QuestionPrompt.jsx
@@ -1,28 +1,58 @@
 import { useState } from 'react';
 import React from 'react';
+import axios from 'axios';
+import { Navigate } from 'react-router-dom';
 
 export const QuestionPrompt = () => {
-    const [inputText, setInputText] = useState('')
-	const [val, setVal] = useState();
-    const handleChange = (e) => {
-    setInputText(e.target.value)
-	e.preventDefault ()
-    }
+    const [values, setValues] = useState({
+		title: '',
+		body: '',
+	});
+
+    const handleTitleChange = (e) => {
+    setValues({...values, title: e.target.value});
+    };
+
+	const handleBodyChange = (e) => {
+	setValues({...values, body: e.target.value});
+	};
+
+	const handleSubmit = (e) => {
+		e.preventDefault()
+		console.log(e)
+		axios
+			.post('https://questionbox-team-lightning.herokuapp.com/api/questions/', {
+				title: values.title,
+				body: values.body,
+	})
+.then((res) => {
+	console.log(res.data)
+})
+};
 
     return (
-    <div className="input-field">
-        <label htmlFor="text-input">Ask a question</label>
-        <input
-        type="text"
-        className="text-input"
-        value={inputText}
-        onChange={handleChange}
-        />
-		<div>
-			<button onClick={() => setVal(() => "")}>clear</button>
+	<form onSubmit={handleSubmit}>
+		<div className="input-field">
+			<label htmlFor="text-input">Question Title</label>
+			<input
+			type="text"
+			className="text-input"
+			values={values.title}
+			onChange={handleTitleChange}
+			/>
+
+			<label htmlFor="text-area">Question Text</label>
+			<input
+			type="text"
+			className="text-area"
+			values={values.body}
+			onChange={handleBodyChange}
+			/>
 		</div>
-        <div>My Question: {inputText}</div>
-    </div>
+		<div>
+			<button type="submit">Submit</button>
+		</div>
+	</form>
     )
 }
 
