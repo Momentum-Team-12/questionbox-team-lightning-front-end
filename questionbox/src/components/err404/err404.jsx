d3b1670b6bdfe19e1bf6d5262050de6ca31dccd2
Stephen Reish <spreish@ncsu.edu>
2022-06-07 15:07:54 -0400

    rudimentary 404

---
+++ questionbox/src/components/err404/err404.jsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import { styled, alpha } from '@mui/material/styles';
+import { Paper, IconButton, Typography, Button } from '@mui/material'
+
+export default function err404() {
+    return (
+        <Paper />
+    )
+}
