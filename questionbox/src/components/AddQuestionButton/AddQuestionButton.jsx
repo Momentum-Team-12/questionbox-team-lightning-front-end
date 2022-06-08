d539ebfb9ba696c0874e35c968fa28e88df2fa60
Stephen Reish <spreish@ncsu.edu>
2022-06-08 16:14:36 -0400

    Small add question button fixes

--- questionbox/src/components/AddQuestionButton/AddQuestionButton.jsx
+++ questionbox/src/components/AddQuestionButton/AddQuestionButton.jsx
@@ -4,16 +4,16 @@ import Box from '@mui/material/Box';
 import Fab from '@mui/material/Fab';
 import AddIcon from '@mui/icons-material/Add';
 
-export default function AddQuestionButton(isLoggedIn) {
-    if (!isLoggedIn) {
-        return (
-            <Box sx={{ '& > :not(style)': { m: 1 } }}>
-                <Fab component={Link} to="/login" color="secondary" aria-label="add">
-                    <AddIcon />
-                </Fab>
-            </Box>
-        )
-    };
+const buttonStyle = {
+    margin: 0,
+    top: 'auto',
+    right: 20,
+    bottom: 20,
+    left: 'auto',
+    position: 'fixed',
+};
+
+export default function AddQuestionButton() {
     return (
         <Box sx={{ '& > :not(style)': { m: 1 } }}>
             <Fab component={Link} to="/questions/add" color="secondary" aria-label="add">
@@ -22,3 +22,24 @@ export default function AddQuestionButton(isLoggedIn) {
         </Box>
     );
 }
+
+// export default function AddQuestionButton(isLoggedIn) {
+//     {
+//         !isLoggedIn ? (
+//         return (
+//             <Box sx={{ '& > :not(style)': { m: 1 } }}>
+//                 <Fab component={Link} to="/login" color="secondary" aria-label="add">
+//                     <AddIcon />
+//                 </Fab>
+//             </Box>)
+//         ) : (
+//         return (
+//             <Box sx={{ '& > :not(style)': { m: 1 } }}>
+//                 <Fab component={Link} to="/questions/add" color="secondary" aria-label="add">
+//                     <AddIcon />
+//                 </Fab>
+//             </Box>
+//         )))
+//     }
+
+// }
