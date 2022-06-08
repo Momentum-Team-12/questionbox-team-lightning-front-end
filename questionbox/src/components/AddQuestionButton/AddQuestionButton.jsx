3d67b864a52feacc773b0befabba01ef15e665a8
Amy Gori <amydgori@gmail.com>
2022-06-08 14:40:31 -0400

    Fix snackbar and rearrange routes

--- questionbox/src/components/AddQuestionButton/AddQuestionButton.jsx
+++ questionbox/src/components/AddQuestionButton/AddQuestionButton.jsx
@@ -1,24 +1,29 @@
-import * as React from 'react';
-import { Link } from "react-router-dom";
-import Box from '@mui/material/Box';
-import Fab from '@mui/material/Fab';
-import AddIcon from '@mui/icons-material/Add';
+import * as React from 'react'
+import { Link } from 'react-router-dom'
+import Box from '@mui/material/Box'
+import Fab from '@mui/material/Fab'
+import AddIcon from '@mui/icons-material/Add'
 
-export default function AddQuestionButton(isLoggedIn) {
-    if (!isLoggedIn) {
-        return (
-            <Box sx={{ '& > :not(style)': { m: 1 } }}>
-                <Fab component={Link} to="/login" color="secondary" aria-label="add">
-                    <AddIcon />
-                </Fab>
-            </Box>
-        )
-    };
+export default function AddQuestionButton(props) {
+  if (!props.isLoggedIn) {
     return (
-        <Box sx={{ '& > :not(style)': { m: 1 } }}>
-            <Fab component={Link} to="/questions/add" color="secondary" aria-label="add">
-                <AddIcon />
-            </Fab>
-        </Box>
-    );
+      <Box sx={{ '& > :not(style)': { m: 1 } }}>
+        <Fab component={Link} to="/login" color="secondary" aria-label="add">
+          <AddIcon />
+        </Fab>
+      </Box>
+    )
+  }
+  return (
+    <Box sx={{ '& > :not(style)': { m: 1 } }}>
+      <Fab
+        component={Link}
+        to="/questions/add"
+        color="secondary"
+        aria-label="add"
+      >
+        <AddIcon />
+      </Fab>
+    </Box>
+  )
 }
